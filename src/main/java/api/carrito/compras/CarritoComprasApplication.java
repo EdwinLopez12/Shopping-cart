a7155bfb5e4127b81d1437e2630b13db4b5c4fde
Edwin <edwin.lopezb.1297@gmail.com>
2021-08-24 17:50:01 -0500

    Initial commit

---
+++ src/main/java/api/carrito/compras/CarritoComprasApplication.java
@@ -0,0 +1,13 @@
+package api.carrito.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoComprasApplication.class, args);
+	}
+
+}
