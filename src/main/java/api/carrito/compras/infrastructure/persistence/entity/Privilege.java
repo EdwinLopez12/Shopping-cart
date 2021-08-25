3035e04070fca0bbc0261a872bd9e06f02cd8651
Edwin <edwin.lopezb.1297@gmail.com>
2021-08-25 00:53:02 -0500

    Entidades para la autenticación

---
+++ src/main/java/api/carrito/compras/infrastructure/persistence/entity/Privilege.java
@@ -0,0 +1,35 @@
+package api.carrito.compras.infrastructure.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tb_privilege")
+public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(targetEntity = Role.class, fetch = FetchType.LAZY, mappedBy = "privileges")
+    private List<Role> roles;
+}
