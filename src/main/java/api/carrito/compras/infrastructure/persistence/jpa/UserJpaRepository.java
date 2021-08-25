a185a14cb6ebf79c096737753a4d9cf4b07449af
Edwin <edwin.lopezb.1297@gmail.com>
2021-08-25 13:11:17 -0500

    Update UserJpaRepository.java

--- src/main/java/api/carrito/compras/infrastructure/persistence/jpa/UserJpaRepository.java
+++ src/main/java/api/carrito/compras/infrastructure/persistence/jpa/UserJpaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserJpaRepository extends JpaRepository<Long, User> {
+public interface UserJpaRepository extends JpaRepository<User, Long> {
 
     @Query(
             value = "SELECT u FROM User u WHERE u.username = :username"
