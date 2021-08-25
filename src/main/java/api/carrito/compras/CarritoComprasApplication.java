e0b2563d8d240d400e43742ddff85d07c8bf1ae0
Edwin <edwin.lopezb.1297@gmail.com>
2021-08-25 13:06:30 -0500

    Definiendo zona horaria por defecto

--- src/main/java/api/carrito/compras/CarritoComprasApplication.java
+++ src/main/java/api/carrito/compras/CarritoComprasApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @Import(SwaggerConfig.class)
 public class CarritoComprasApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
 		SpringApplication.run(CarritoComprasApplication.class, args);
 	}
 
