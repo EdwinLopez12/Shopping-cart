6a5db9951032e408a5a5f8277cac9baa082a6e72
Edwin López <edwin.lopezb.1297@gmail.com>
2021-09-08 14:31:29 -0500

    Update messages

--- src/main/java/api/carrito/compras/domain/dto/auth/RegisterUserRequest.java
+++ src/main/java/api/carrito/compras/domain/dto/auth/RegisterUserRequest.java
@@ -21,23 +21,23 @@ import javax.validation.constraints.NotBlank;
 public class RegisterUserRequest {
 
     @JsonProperty
-    @NotBlank(message = "El nombre de usuario es requerido")
-    @Length(min = 3, max = 255, message = "El nombre de usuario debe de ser mayor o igual a 3")
+    @NotBlank(message = "Username es required")
+    @Length(min = 3, max = 255, message = "Username must be greater than or equal to 3")
     private String username;
 
     @JsonProperty
-    @NotBlank(message = "El email es requerido")
-    @Email(message = "El correo electrónico debe tener el formato correcto")
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be in the correct format")
     private String email;
 
     @JsonProperty
-    @NotBlank(message = "La contraseña es requerida")
-    @Length(min = 6, max = 255, message = "La contraseña debe de ser mayor o igual a 6")
+    @NotBlank(message = "Password is required")
+    @Length(min = 6, max = 255, message = "Password must be greater than or equals to 6")
     private String password;
 
     @JsonProperty
-    @NotBlank(message = "La verificación de contraseña es requerida")
-    @Length(min = 6, max = 255, message = "La verificación de contraseña debe de ser mayor o igual a 6")
+    @NotBlank(message = "Verification password is required")
+    @Length(min = 6, max = 255, message = "Verification password must be greater than or equals to 6")
     private String passwordVerify;
 }
 
