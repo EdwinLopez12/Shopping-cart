70be2396a96c796e2ebe874a2d2d4ed3af3a80c7
Edwin López <edwin.lopezb.1297@gmail.com>
2021-09-08 13:49:45 -0500

    Update MailData.java

--- src/main/java/api/carrito/compras/domain/utils/MailData.java
+++ src/main/java/api/carrito/compras/domain/utils/MailData.java
@@ -4,10 +4,10 @@ public final class MailData {
 
     private MailData() {}
 
-    public static final String SUBJECT ="Carrito compras";
-    public static final String TITLE = "¡Hola! ";
-    public static final String BODY_SIGNUP = "Gracias por registrarte. Da click en el enlace para activar la cuenta.";
-    public static final String BODY_RESET_PASSWORD = "Estas leyendo este mensaje porque recibimos una solucitud de cambio de contraseña desde tu cuenta. Si no lo hiciste no se requieren más acciones, pero si lo hiciste, por favor has click en la siguiente URL para restablecer su contraseña";
+    public static final String SUBJECT ="Shopping Cart";
+    public static final String TITLE = "Hi! ";
+    public static final String BODY_SIGNUP = "Thanks for signing up. Click on the link to activate the account.";
+    public static final String BODY_RESET_PASSWORD = "You are reading this message because we received a request to change your password from your account. If you didn't, no further action is required, but if you did, please click on the following URL to reset your password";
     public static final String END_POINT_SIGNUP = "/account-verification";
     public static final String END_POINT_RESET_PASSWORD = "/reset/password";
 }
