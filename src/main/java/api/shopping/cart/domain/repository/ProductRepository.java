a161791b086fe7bf225c3ba8c0a44c4cc269df06
Edwin López <edwin.lopezb.1297@gmail.com>
2021-09-20 15:02:28 -0500

    Update ProductRepository.java

--- src/main/java/api/shopping/cart/domain/repository/ProductRepository.java
+++ src/main/java/api/shopping/cart/domain/repository/ProductRepository.java
@@ -4,7 +4,7 @@ package api.shopping.cart.domain.repository;
  * ProductRepository class
  *
  * @author edwin.lopezb.1297
- * @project cart
+ * @project shoppingcart
  * @since v1.0.0 - sep. 2021
  */
 public interface ProductRepository {
