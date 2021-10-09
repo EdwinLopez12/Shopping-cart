5bb605b58712c0beaadc942a10ad22bfbd193e63
Edwin López <edwin.lopezb.1297@gmail.com>
2021-10-08 19:27:25 -0500

    remove sensitive data

--- src/main/java/api/shopping/cart/domain/usecase/impl/PaypalService.java
+++ src/main/java/api/shopping/cart/domain/usecase/impl/PaypalService.java
@@ -2,7 +2,6 @@ package api.shopping.cart.domain.usecase.impl;
 
 import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,13 +14,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaypalService {
 
-//    @Value("${paypal.client.id}")
-//    private String applicationClientId;
-//    @Value("${paypal.client.secret}")
-//    private String applicationClientSecret;
-
-    private final String applicationClientId = "Ac6Qn4RgzT12LSnrbhC7Kadthx5WPNjNAkp6HPhkSvN-vUF8F6HeKPjPN4tZvfK5LmS8WGFqXGiAdfJZ";
-    private final String applicationClientSecret = "EEvW3QIeZa2ojepPH--Jbsog8Li8-pVlCl2DMNae-Ne4Sew8EZEayHflSBBiFCMOx--Y9SmaHU7khoiH";
+    private final String applicationClientId = "";
+    private final String applicationClientSecret = "";
 
     /**
      *Set up the PayPal Java SDK environment with PayPal access credentials.
